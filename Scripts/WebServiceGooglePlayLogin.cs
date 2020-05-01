37e774cbead7cc80b37cddfd939865abcedfd014
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2020-05-02 03:07:47 +0700

    Implement google play login client for web service client

---
+++ Scripts/WebServiceGooglePlayLogin.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(WebServiceClient))]
+public class WebServiceGooglePlayLogin : BaseGooglePlayLoginService
+{
+    private WebServiceClient webServiceClient;
+
+    private void Awake()
+    {
+        webServiceClient = GetComponent<WebServiceClient>();
+    }
+
+    public override void LoginWithGooglePlay(string idToken, UnityAction<PlayerResult> onFinish)
+    {
+        var dict = new Dictionary<string, object>();
+        dict.Add("idToken", idToken);
+        webServiceClient.PostAsDecodedJSON<PlayerResult>("/login-with-google-play", (www, result) =>
+        {
+            onFinish(result);
+        }, JsonConvert.SerializeObject(dict));
+    }
+}
