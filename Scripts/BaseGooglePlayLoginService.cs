aea57cb2ec602c81d4010ce1c298f8b1e566ab05
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2020-05-02 02:50:07 +0700

    Add base google play login service

---
+++ Scripts/BaseGooglePlayLoginService.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public abstract class BaseGooglePlayLoginService : MonoBehaviour
+{
+    public abstract void LoginWithGooglePlay(string idToken, UnityAction<PlayerResult> onFinish);
+}
